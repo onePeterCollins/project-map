8e5ad77fdbecccc433608f815f02243062acaf42
onePeterCollins <onepetercollins@gmail.com>
2022-07-12 03:43:56 +0400

    Placeholder

---
+++ build/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author - Peter Collins
+ * @description - Optimized and minified production ready code will be released here
+ * @file - This file will export all the magic
+ * @see - "../index.js"
+ */
+
+
---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * @author - Peter Collins
+ * @description - Entry point
+ * @file - This file is a placeholder for what is to come
+ */
+
+
